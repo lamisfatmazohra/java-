824f71a2e78846ceade714812c2cdc8cfa06e508
lamisfatmazohra <89184286+lamisfatmazohra@users.noreply.github.com>
2022-04-10 01:07:00 +0100

    add connection to BDD

---
+++ Table UI/src/Configs.java
@@ -0,0 +1,8 @@
+public class Configs {
+    protected static String dbHost = "localhost";
+    protected static String dbPort = "3306";
+    protected static String dbUser = "root";
+    protected static String dbPass = "root";
+    protected static String dbName = "Test";
+
+}
--- Table UI/src/TableController.java
+++ Table UI/src/TableController.java
@@ -1,6 +1,13 @@
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Handler;
+
+import com.mysql.cj.xdevapi.Result;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -40,9 +47,8 @@ public class TableController<Button> implements Initializable {
     public void initialize(URL arg0, ResourceBundle arg1) {
         // TODO Auto-generated method stub
         personneData = FXCollections.observableArrayList();
-        personneData.add(new Personne("fatma zohra", "lamis", 20));
-        personneData.add(new Personne("bounouh", "nesrine", 24));
-        personneData.add(new Personne("douaa", "yousra", 19));
+        this.verifierSiVide();
+        this.recupererPersonne();
         this.nomcol.setCellValueFactory(cellData -> cellData.getValue().getNomProperty());
         this.prenomcol.setCellValueFactory(cellData -> cellData.getValue().getPrenomProperty());
         this.agecol.setCellValueFactory(cellData -> cellData.getValue().getAgeProperty());
@@ -66,5 +72,84 @@ public class TableController<Button> implements Initializable {
         this.nom.setText("");
         this.prenom.setText("");
         this.age.setText("");
+        final Connection connection;
+        DBHundler hundler = new DBHundler();
+        PreparedStatement pst;
+        String insert = "INSERT INTO personne(Nom,prenom,age)" + "VALUES (?,?,?)";
+        connection = (Connection) hundler.getConnection();
+        try {
+            pst = (PreparedStatement) connection.prepareStatement(insert);
+            pst.setString(1, nom); // Si le type de la colonne est string
+            pst.setString(2, prenom); // Si le type de la colonne est int
+            pst.setInt(3, Integer.valueOf(age));
+            pst.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void remplirBase() {
+        final Connection connection;
+        DBHundler hundler = new DBHundler();
+        PreparedStatement pst;
+        String insert = "INSERT INTO personne(Nom,prenom,age)" + "VALUES (?,?,?)";
+        connection = (Connection) hundler.getConnection();
+        try {
+            for (int i = 0; i < 10; i++) {
+                pst = (PreparedStatement) connection.prepareStatement(insert);
+                pst.setString(1, "nom" + i); // Si le type de la colonne est string
+                pst.setString(2, "prenom" + i); // Si le type de la colonne est int
+                pst.setInt(3, i + 30);
+                pst.executeUpdate();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void recupererPersonne() {
+        final Connection connection;
+        DBHundler hundler = new DBHundler();
+        PreparedStatement pst;
+        String select = "select * from personne";
+        connection = hundler.getConnection();
+        try {
+            pst = (PreparedStatement) connection.prepareStatement(select);
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                String nom = rs.getString("Nom");
+                String prenom = rs.getString("prenom");
+                int age = rs.getInt("age");
+                this.personneData.add(new Personne(nom, prenom, age));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void verifierSiVide() {
+        final Connection connection;
+        DBHundler hundler = new DBHundler();
+        PreparedStatement pst;
+        String select = "select count(*) as taille from personne";
+        connection = hundler.getConnection();
+        try {
+            pst = (PreparedStatement) connection.prepareStatement(select);
+            ResultSet rs = pst.executeQuery();
+            if (rs.next()) {
+                int taille = rs.getInt("taille");
+                if (taille == 0) {
+                    this.remplirBase();
+                }
+            } else {
+                this.remplirBase();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
